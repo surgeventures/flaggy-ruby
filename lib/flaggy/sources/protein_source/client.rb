c23ae057b8b24a9b682570166fb1d590477cdef3
Karol Słuszniak <ksluszniak@gmail.com>
2018-01-26 18:00:36 +0100

    Comment on regenerating protos

--- lib/flaggy/sources/protein_source/client.rb
+++ lib/flaggy/sources/protein_source/client.rb
@@ -1,3 +1,11 @@
+# In order to re-generate changed protos, do the following:
+#
+# - run 'bundle exec irb -I lib'
+# - require 'protein'
+# - require 'shellwords'
+# - call Protein::ProtoCompiler.call(proto_directory: "./lib/flaggy/sources/protein_source/proto")
+# - wrap new files in module Flaggy; class ProteinSource; ...; end; end
+
 module Flaggy
 class ProteinSource
 class Client < ::Protein::Client
